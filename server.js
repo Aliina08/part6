fa6d06d8670fa6d511a93795f2c0df255e032024
Aliina08 <abdulhalikova.lina@yandex.ru>
2021-05-31 09:40:48 +0300

    Adding the Mongo module. And adding connection to the data base in Mongo

--- server.js
+++ server.js
@@ -1,25 +1,56 @@
 var express = require("express"),
 http = require("http"),
-app = express(),
-toDos = [
-// настраиваем список задач копированием
-// содержимого из файла todos.OLD.json
-];
+// импортируем библиотеку mongoose
+mongoose = require("mongoose"),
+app = express();
+// Это модель Mongoose для задач
+
 app.use(express.static(__dirname + "/client"));
+var ToDoSchema = mongoose.Schema({
+description: String,
+tags: [ String ]
+});
+var ToDo = mongoose.model("ToDo", ToDoSchema);
+// начинаем слушать запросы
 http.createServer(app).listen(3000);
 // этот маршрут замещает наш файл
 // todos.json в примере из части 5
 app.get("/todos.json", function (req, res) {
-res.json(toDos);
+	ToDo.find({}, function (err, toDos) {
+		res.json(toDos);
+	});
 });
 // командуем Express принять поступающие
 // объекты JSON
 app.use(express.urlencoded({ extended: true }));
+// подключаемся к хранилищу данных Amazeriffic в Mongo
+mongoose.connect('mongodb://localhost/Amazeriffic', {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true 
+	}).then(res => {
+		console.log("DB Connected!")
+	}).catch(err => {
+		console.log(Error, err.message);
+	});
 app.post("/todos", function (req, res) {
-	// сейчас объект сохраняется в req.body
-	var newToDo = req.body;
-	console.log(newToDo);
-	toDos.push(newToDo);
-// простой объект отправлен обратно
-	res.json({"message":"Вы размещаетесь на сервере!"});
+	console.log(req.body);
+	var newToDo = new ToDo({"description":req.body.description,
+	"tags":req.body.tags});
+	newToDo.save(function (err, result) {
+		if (err !== null) {
+			console.log(err);
+			res.send("ERROR");
+		} else {
+			// клиент ожидает, что будут возвращены все задачи,
+			// поэтому для сохранения совместимости сделаем дополнитель) в папку part7 . Нам понадобится файл package.json , гденый запрос
+			ToDo.find({}, function (err, result) {
+				if (err !== null) {
+					// элемент не был сохранен
+					res.send("ERROR");
+				}
+				res.json(result);
+			});
+		}
+	});
 });
