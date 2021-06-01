f873ecee749c6684c0fff03b9385a7afaecee8d3
Aliina08 <abdulhalikova.lina@yandex.ru>
2021-06-01 13:43:12 +0300

    Configurin routes (add todos_controller.js and todo.js in models)

--- server.js
+++ server.js
@@ -2,24 +2,15 @@ var express = require("express"),
 http = require("http"),
 // импортируем библиотеку mongoose
 mongoose = require("mongoose"),
+ToDosController = require("./controllers/todos_controller.js"),
+UsersController = require("./controllers/users_controller.js"),
 app = express();
 // Это модель Mongoose для задач
 
-app.use(express.static(__dirname + "/client"));
-var ToDoSchema = mongoose.Schema({
-description: String,
-tags: [ String ]
-});
-var ToDo = mongoose.model("ToDo", ToDoSchema);
+
 // начинаем слушать запросы
 http.createServer(app).listen(3000);
-// этот маршрут замещает наш файл
-// todos.json в примере из части 5
-app.get("/todos.json", function (req, res) {
-	ToDo.find({}, function (err, toDos) {
-		res.json(toDos);
-	});
-});
+
 // командуем Express принять поступающие
 // объекты JSON
 app.use(express.urlencoded({ extended: true }));
@@ -33,24 +24,17 @@ mongoose.connect('mongodb://localhost/Amazeriffic', {
 	}).catch(err => {
 		console.log(Error, err.message);
 	});
-app.post("/todos", function (req, res) {
-	console.log(req.body);
-	var newToDo = new ToDo({"description":req.body.description,
-	"tags":req.body.tags});
-	newToDo.save(function (err, result) {
-		if (err !== null) {
-			console.log(err);
-			res.send("ERROR");
-		} else {
-			// клиент ожидает, что будут возвращены все задачи,
-			// поэтому для сохранения совместимости сделаем дополнитель) в папку part7 . Нам понадобится файл package.json , гденый запрос
-			ToDo.find({}, function (err, result) {
-				if (err !== null) {
-					// элемент не был сохранен
-					res.send("ERROR");
-				}
-				res.json(result);
-			});
-		}
-	});
-});
+
+app.get("/users.json", UsersController.index); 
+app.post("/users", UsersController.create); 
+app.get("/users/:username", UsersController.show);
+app.put("/users/:username", UsersController.update);
+app.delete("/users/:username", UsersController.destroy);
+
+app.get("/user/:username/todos.json", ToDosController.index);
+app.post("/user/:username/todos", ToDosController.create);
+app.put("/user/:username/todos/:id", ToDosController.update);
+app.delete("/user/:username/todos/:id", ToDosController.destroy);
+
+app.use('/', express.static(__dirname + '/client'));
+app.use('/user/:username', express.static(__dirname + '/client'));
