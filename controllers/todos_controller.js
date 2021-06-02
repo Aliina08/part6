f2639b26e558b04f240ba55a819022bb14c7e05a
Aliina08 <abdulhalikova.lina@yandex.ru>
2021-06-02 07:25:38 +0300

    Adding deletion of task lists

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -21,11 +21,11 @@ ToDosController.index = function (req, res) {
 			} else if (result.length === 0) {
 				res.status(404).json({"result_length": 0});
 			} else {
-				respondWithToDos({});
+				respondWithToDos({"owner": result[0]._id});
 			}
 		});
 	} else {
-		res.status(404).json({"username": username});	}
+		respondWithToDos({});	}
 };
 // Отобразить пользователя
 ToDosController.show = function (req, res) {
@@ -34,8 +34,27 @@ res.send(200);
 };
 // Создать нового пользователя
 ToDosController.create = function (req, res) {
-console.log("вызвано действие: создать");
-res.send(200);
+	var username = req.params.username || null,
+		newToDo = new ToDo({"description": req.body.description,
+			"tags": req.body.tags});
+	User.find({"username": username}, function (err, result) {
+		if (err) {
+			res.send(500);
+		} else {
+			if (result.length === 0) {
+				newToDo.owner = null;
+			} else {
+				newToDo.owner = result[0]._id;
+			}
+			newToDo.save(function (err, result) {
+				if (err !== null) {
+					res.json(500, err);
+				} else {
+					res.status(200).json(result);
+				}
+			});
+		}	
+	});
 };
 // Обновить существующего пользователя
 ToDosController.update = function (req, res) {
@@ -44,7 +63,17 @@ res.send(200);
 };
 // Удалить существующего пользователя
 ToDosController.destroy = function (req, res) {
-console.log("destroy action called");
-res.send(200);
+	var id = req.params.id;
+	ToDo.deleteOne({"_id": id}, function (err, todo) {
+		if (err !== null) {
+			res.status(500).json(err);
+		} else {
+			if (todo.n === 1 && todo.ok === 1 && todo.deletedCount === 1) {
+				res.status(200).json(todo);
+			} else {
+				res.status(404).json({"status": 404});
+			}
+		}
+	});
 };
 module.exports = ToDosController;
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,6 +3,24 @@ var User = require("../models/user.js"),
 	mongoose = require("mongoose");
 var UsersController = {};
 
+// проверка, не существует ли уже пользователь
+User.find({}, function (err, result) {
+if (err !== null) {
+console.log("Что-то идет не так");
+console.log(err);
+} else if (result.length === 0) {
+console.log("Создание тестового пользователя...");
+var exampleUser = new User({"username":"usertest"});
+exampleUser.save(function (err, result) {
+if (err) {
+console.log(err);
+} else {
+console.log("Тестовый пользователь сохранен");
+}
+});
+}
+});
+
 UsersController.index = function (req, res) {
 console.log("вызвано действие: индекс");
 res.send(200);
@@ -27,22 +45,7 @@ UsersController.destroy = function (req, res) {
 console.log("destroy action called");
 res.send(200);
 };
-module.exports = UsersController;
 
-// проверка, не существует ли уже пользователь
-User.find({}, function (err, result) {
-if (err !== null) {
-console.log("Что-то идет не так");
-console.log(err);
-} else if (result.length === 0) {
-console.log("Создание тестового пользователя...");
-var exampleUser = new User({"username":"usertest"});
-exampleUser.save(function (err, result) {
-if (err) {
-console.log(err);
-} else {
-console.log("Тестовый пользователь сохранен");
-}
-});
-}
-});
+
+
+module.exports = UsersController;
